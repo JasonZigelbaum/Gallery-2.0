3f64a755fec22bc8472701622d66e1b702173d11
Jason Zigelbaum <jasonzigelbaum@gmail.com>
2012-06-22 12:23:32 -0400

    added sidebars...

--- js/gallery2.0.js
+++ js/gallery2.0.js
@@ -4,22 +4,50 @@
     Gallery = function () {
         /*  Set up arguments in any order    */
         var args            = arguments[0],
-        id                  = args.id,
-        transition_in       = args.transition_in,
-        transition_out      = args.transition_out,
-        images              = args.images,
-        timer               = args.timer || 5000,
-        cur_index           = 0,
+            id              = args.id,
+            transition_in   = args.transition_in,
+            transition_out  = args.transition_out,
+            images          = args.images,
+            timer           = args.timer || 5000,
+            cur_index       = 0,
         /*  Add canvas and context    */
-        canvas              = function() {
-            var canvas      = document.createElement("canvas");
-            document.getElementById(id).appendChild(canvas);
-            return canvas
-        }(),
-        context              = canvas.getContext('2d');
+            elements        = function() {
 
+            var wrapper     = document.createElement("div"),
+                canvas      = document.createElement("canvas"),
+                left        = document.createElement("div"),
+                right       = document.createElement("div");
+                
+            wrapper.id = "gallery_wrapper";
+            left.id = "left";
+            right.id = "right";
+            left.addEventListener("click", function() { setTimeout(go_left(), 5000) }, false);
+            right.addEventListener("click", function() { setTimeout(go_right(), 5000) }, false);
+            wrapper.addEventListener("mouseover", 
+                function() { 
+                    left.style.opacity  = 1;
+                    right.style.opacity = 1; 
+                }
+                , false);
+            wrapper.addEventListener("mouseout", 
+                function() { 
+                    left.style.opacity  = 0;
+                    right.style.opacity = 0; 
+                }
+                , false);
+            document.getElementById(id).appendChild(wrapper)
+            wrapper.appendChild(left);
+            wrapper.appendChild(canvas);            
+            wrapper.appendChild(right);
+            return { canvas : canvas, wrapper : wrapper };
+        }(),
+            wrapper             = elements.wrapper,
+            canvas              = elements.canvas,
+            context             = canvas.getContext('2d'),
+            interval;
+        
         /*  Set up our private methods    */
-        var draw_new_image = function () {
+        var draw_next_image = function () {
             cur_index = ++cur_index % images.length;
             context.clearRect(0, 0, canvas.width, canvas.height);
             img = new Image();
@@ -33,7 +61,25 @@
             img.src = images[cur_index];
         };
 
+        var draw_prev_image = function () {
+            cur_index = Math.abs(--cur_index % images.length);
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            img = new Image();
+            img.onload = function() {
+                adjust_canvas();
+                context.drawImage( img, 0, 0, img.width, img.height );
+                var copy = Animations.Helpers.copy_image(img);
+                Animations.Helpers.clear_image(img);
+                Animations.animate( context, img, copy, transition_in, undefined);
+            }
+            img.src = images[cur_index];
+        };
+        
         var adjust_canvas = function() {
+            wrapper.style.width     = [img.width + 100 ,'px'].join('');
+            wrapper.style.height    = [img.height + 16,'px'].join('');
+            wrapper.width           = img.width + 100;
+            wrapper.height          = img.height;
             canvas.style.width     = [img.width ,'px'].join('');
             canvas.style.height    = [img.height ,'px'].join('');
             canvas.width           = img.width;
@@ -51,14 +97,32 @@
             img.src = images[cur_index];
             
             /*  Set the interval    */
-            setInterval(loop, timer);
+            interval = setInterval(loop, timer);
         };
 
         var loop = function () {
             /*  Trigger slide   */
-            Animations.animate( context, img, undefined, transition_out, draw_new_image );
+            Animations.animate( context, img, undefined, transition_out, draw_next_image );
         };
 
+        /*  Add event listener function objects    */
+        var go_left = function() {
+            /*  Stop the loop    */
+            clearInterval(interval);
+            /*  Go back and animate.    */
+            Animations.animate( context, img, undefined, transition_out, draw_next_image );
+            /*  Set the interval again    */
+            interval = setInterval(loop, timer);
+        }
+        
+        var go_right = function() {
+            /*  Stop the loop    */
+            clearInterval(interval);
+            /*  Go back and animate.    */
+            Animations.animate( context, img, undefined, transition_out, draw_prev_image );
+            /*  Set the interval again    */
+            interval = setInterval(loop, timer);
+        }
         /*  Reveal our public methods using the revealing module pattern.    */
         return {
             init : init
